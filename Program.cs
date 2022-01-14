08186c530e2957e2eedbf03c858dff850853f643
hjkyong <egdc1047@snu.ac.kr>
2022-01-14 11:32:35 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace git_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please Github is needed to done");
+        }
+    }
+}
